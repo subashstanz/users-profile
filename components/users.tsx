92f372717fec53f00720ba741f2ce32a6836e42c
subashstanz <subash.shaheerarjun@gmail.com>
2023-01-07 15:28:17 +0530

    [WIP] Wip of users page

---
+++ components/users.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react'
+
+type Props = {}
+
+function Users({}: Props) {
+  useEffect(() => {
+
+  },[])
+
+  const getusers = () => {
+    
+  }
+  return (
+    <div className='bg-white h- w-full flex items-center justify-center'>
+      <div className='w-[500px] h-[500px] bg-gray-400'>
+        <div> Select an Account</div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Users
